cf67057d9691a69b16645a4a7694b37b1044c066
mallaji <milanmalla29@gmail.com>
2024-06-18 21:50:09 +0545

    New Changes

--- orangehrmtests/adduser.test.js
+++ orangehrmtests/adduser.test.js
@@ -1,13 +1,21 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/orangelogin');
+const Adduser = require('../pages/adduser')
 
-test.use({ headless: true }); // Set the browser to run in headless mode
+//test.use({ headless: true }); 
 
-test("Valid Login", async function({ page }) {
+test("Add Admin User", async function({ page }) {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.login('Admin', 'admin123'); // Add the user credentials for username and password
     await expect(page).toHaveURL(/dashboard/);
+
+    // Add Admin
+    const adduser= new Adduser(page);
+    await adduser.addadmin();
+
+    //Verify 
+    await adduser.verifyaddusertitle()
 });
 
 
--- pages/adduser.js
+++ pages/adduser.js
@@ -1,3 +1,5 @@
+const {expect} = require("@playwright/test")
+
 const dotenv = require('dotenv');
 const env = process.env.NODE_ENV || 'dev';
 dotenv.config({ path: `./env/.env.${env}` });
@@ -5,16 +7,22 @@ dotenv.config({ path: `./env/.env.${env}` });
 class Adduser {
     constructor(page) {
         this.page = page;
-        this.searchid = "//input[@class='oxd-input oxd-input--active' and @placeholder='Search']";
         this.adminid = "//a[@class='oxd-main-menu-item' and @href='/web/index.php/admin/viewAdminModule']//span[text()='Admin']";
-        this.subidSelector = "//button[contains(@class, 'oxd-button--main') and contains(@class, 'orangehrm-login-button')]";
+        this.addbuttonid = "(//button[contains(@class, 'oxd-button') and contains(@class, 'oxd-button--medium') and contains(@class, 'oxd-button--secondary')])[2]";
+        this.addusertitle="//h6[text()='Add User']";
+        this.roleid="(//div[contains(@class, 'oxd-select-text') and contains(@class, 'oxd-select-text--active')])[1]";
     }
 
-    async login(username, password) {
-        await this.page.fill(this.useridSelector, username, { delay: 70 });
-        await this.page.fill(this.passidSelector, password, { delay: 70 });
-        await this.page.click(this.subidSelector);
+    async verifyaddusertitle(){
+        await expect(this.page.locator(this.addusertitle)).toBeVisible()
+    }
+
+    async addadmin() {
+        await this.page.click(this.adminid);
+        await this.page.click(this.addbuttonid);
+        await this.page.pause();
+        //await this.page.click(this.subidSelector);
     }
 }
 
-module.exports = LoginPage;
+module.exports = Adduser;
