11314ff4987976afaecab29bef2ee9215b337867
Milan Malla <milanmalla29@gmail.com>
2024-12-05 10:04:14 +0545

    added unasiigned

--- gibl/claimunassigned.test.js
+++ gibl/claimunassigned.test.js
@@ -6,11 +6,9 @@ const ele = JSON.parse(JSON.stringify(require("./elements.json")));
 test("Process Reports", async function({ page }) {
     await login(page);
     await expect(page).toHaveURL(/dashboard/)
-    await page.locator(ele.main_menu).click()
-    //await page.pause();
-    await page.locator(ele.search1).fill("Process Reports", { delay: 70 })
-    await page.keyboard.press('Enter')
-    await page.locator(ele.process_reports).click()
-    await expect(page).toHaveURL(/generic/)
+    await page.locator(ele.unassignedclick).click()
+    await expect(page).toHaveURL(/unassigned/)
+    await page.getByPlaceholder(ele.palceholder).type("Merchant Acquisition");
+    await page.keyboard.press('Enter');
     await page.pause();
 });
